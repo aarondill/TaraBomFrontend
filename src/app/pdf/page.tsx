20b8673045af3bd1089088bdb0adb2d8e367929d
Aaron Dill <aaronsacks2006@gmail.com>
2025-07-31 14:44:42 -0500

    fix: next rules

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const PDFViewer = dynamic(() => import("./PDFViewer"), { ssr: false });
 
-export default function Page() {
+function PDFPage() {
 	const searchParams = useSearchParams();
 	const docUrl = searchParams.get("docUrl");
 	if (!docUrl) return <div>No document URL provided</div>;
 	if (typeof docUrl !== "string")
 		return <div>Document URL must be a string</div>;
 	return <PDFViewer docUrl={docUrl} />;
+}
+export default function Page() {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<PDFPage />
+		</Suspense>
+	);
 }

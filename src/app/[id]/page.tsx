457b5deb6226b0fdadbaca3208305c6c8f8548db
Aaron Dill <aaronsacks2006@gmail.com>
2025-07-30 21:02:02 -0500

    feat: fetch from the server

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,8 @@
-// import cssText from "bundle-text:./styles.css";
-/* <style>{cssText}</style> */
+import { isNativeError } from "node:util/types";
+import { Output } from "./types";
+
+const RETRIEVER_SERVER_ADDRESS = process.env.RETRIEVER_SERVER_ADDRESS;
+
 function Table() {
 	// TODO: Implement table
 	return (
@@ -26,14 +29,37 @@ function Table() {
 	);
 }
 
-export default async function App(props: { params: Promise<{ id?: string }> }) {
+export default async function App(props: { params: Promise<{ id: string }> }) {
 	const { id } = await props.params;
 
-	if (!id) return null;
+	const url = new URL(id, RETRIEVER_SERVER_ADDRESS);
+	let response;
+
+	try {
+		response = await fetch(url);
+	} catch (e) {
+		if (
+			isNativeError(e) &&
+			isNativeError(e.cause) &&
+			"code" in e.cause &&
+			e.cause.code === "ECONNREFUSED"
+		) {
+			return <div>Error: Could not connect to retriever server</div>;
+		}
+		return <div>Error: {isNativeError(e) ? e.message : String(e)}</div>;
+	}
+
+	const status = response.status;
+	if (status !== 200) {
+		return <div>Error: {await response.text()}</div>;
+	}
+
+	const data = (await response.json()) as Output;
 	return (
 		<div>
 			<div>
 				<h1>Get Started with this id: {id}</h1>
+				results: {JSON.stringify(data)}
 				<Table />
 			</div>
 		</div>
